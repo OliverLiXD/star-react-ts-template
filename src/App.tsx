e6d97458d634cb127254f9218cddbf2703b9691e
laymanli <1009641543@qq.com>
2022-09-11 13:23:45 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import "./App.scss";
+
+
+function App() {
+
+  return (
+    <div className="App">
+      Hello, world!
+    </div>
+  );
+}
+
+export default App;
